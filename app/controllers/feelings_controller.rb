0957ad5171ebea36abf8a1831c41226c2069b320
Paul Campbell <paul@rslw.com>
2009-02-08 16:43:03 +0000

    and bring the views up to speed with stuffing

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -3,7 +3,7 @@ class FeelingsController < ApplicationController
   before_filter :check_settings
   
   def index
-    @posts = Post.all
+    @posts = Post.all(:order => 'created_at DESC')
   end
   
 end
