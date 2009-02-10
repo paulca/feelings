a31ef05aa30fd25df45cd6a068daaaa0e138c888
Paul Campbell <paul@rslw.com>
2009-02-10 14:43:45 +0000

    atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :install, :controller => 'install'
   map.root :controller => "feelings"
   map.admin '/admin', :controller => 'posts', :action => 'new'
-  map.feed '/atom/feed.:format', :controller => 'feelings', :action => 'index'
+  map.feed '/feed/atom.:format', :controller => 'feelings', :action => 'index'
   map.slug "/feeling/:slug", :controller => 'feelings', :action => 'show', :slug => /.*/
 end
